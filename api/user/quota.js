de6f2ad11ba7fb140eae6a3534ad62700ba8dcf1
mislgom <mislgom486@gmail.com>
2026-01-21 15:31:35 +0900

    Refactor JWT token verification to use headers.get

--- api/user/quota.js
+++ api/user/quota.js
@@ -27,7 +27,7 @@ export default async function handler(request) {
     }
 
     // JWT 토큰 검증
-    const authHeader = request.headers['authorization'] || request.headers['Authorization'];
+    const authHeader = request.headers.get('Authorization');
 
     if (!authHeader?.startsWith('Bearer ')) {
         return new Response(
